6c07df285eea3f82bd1f14e33561cbd52b1a7d42
igalklebanov <igalklebanov@gmail.com>
2024-12-29 02:43:21 +0200

    unit tests pt. 2.

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,8 +11,9 @@ import {
 	type SqlBool,
 	sql,
 } from 'kysely'
-import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest'
 import { KyselyReplicationDialect } from '../src/index.js'
+import { RandomReplicaStrategy } from '../src/strategy/random.js'
 import { RoundRobinReplicaStrategy } from '../src/strategy/round-robin.js'
 
 interface Database {
@@ -23,145 +24,209 @@ interface Database {
 	}
 }
 
-describe('kysely-replication: round-robin', () => {
-	let db: Kysely<Database>
-	const executions: string[] = []
+const randomSpy = vi.spyOn(global.Math, 'random')
+const warnSpy = vi.spyOn(global.console, 'warn')
 
-	beforeAll(() => {
-		db = new Kysely({
-			dialect: new KyselyReplicationDialect({
-				primaryDialect: getDummyDialect('primary', executions),
-				replicaDialects: new Array(3)
-					.fill(null)
-					.map((_, i) =>
-						getDummyDialect(`replica-${i}`, executions),
-					) as unknown as readonly [Dialect, ...Dialect[]],
-				replicaStrategy: new RoundRobinReplicaStrategy({}),
-			}),
+describe.each(
+	(['error', 'warn'] as const).flatMap((onTransaction) => [
+		{
+			onTransaction,
+			replicaAssertion: (executions: string[]) =>
+				expect(executions).toEqual([
+					'replica-0',
+					'replica-1',
+					'replica-2',
+					'replica-0',
+					'replica-1',
+				]),
+			Strategy: RoundRobinReplicaStrategy,
+		},
+		{
+			onTransaction,
+			replicaAssertion: (executions: string[]) =>
+				expect(randomSpy).toHaveBeenCalledTimes(executions.length),
+			Strategy: RandomReplicaStrategy,
+		},
+	]),
+)(
+	'kysely-replication: $Strategy.name (onTransaction: $onTransaction)',
+	({ onTransaction, replicaAssertion, Strategy }) => {
+		let db: Kysely<Database>
+		const executions: string[] = []
+		let primaryDialect: Dialect
+		let replicaDialects: readonly [Dialect, ...Dialect[]]
+
+		beforeAll(() => {
+			primaryDialect = getDummyDialect('primary', executions)
+			replicaDialects = new Array(3)
+				.fill(null)
+				.map((_, i) =>
+					getDummyDialect(`replica-${i}`, executions),
+				) as unknown as readonly [Dialect, ...Dialect[]]
+			db = new Kysely({
+				dialect: new KyselyReplicationDialect({
+					primaryDialect,
+					replicaDialects,
+					replicaStrategy: new Strategy({ onTransaction }),
+				}),
+			})
 		})
-	})
 
-	afterEach(() => {
-		executions.length = 0 // clear executions
-	})
+		afterEach(() => {
+			executions.length = 0 // clear executions
+		})
 
-	it('should use primary dialect for DDL queries', async () => {
-		const queries = {
-			alterTable: db.schema
-				.alterTable('users')
-				.addColumn('nickname', 'varchar'),
-			createIndex: db.schema
-				.createIndex('users_index')
-				.on('users')
-				.column('email'),
-			createSchema: db.schema.createSchema('moshe'),
-			createTable: db.schema
-				.createTable('cakes')
-				.addColumn('id', 'serial', (cb) => cb.primaryKey()),
-			createType: db.schema
-				.createType('cake_type')
-				.asEnum(['chocolate', 'vanilla']),
-			createView: db.schema
-				.createView('users_view')
-				.as(db.selectFrom('users').selectAll()),
-			dropIndex: db.schema.dropIndex('users_index'),
-			dropSchema: db.schema.dropSchema('moshe'),
-			dropTable: db.schema.dropTable('cakes'),
-			dropType: db.schema.dropType('cake_type'),
-			dropView: db.schema.dropView('users_view'),
-		} satisfies {
-			[K in keyof Omit<SchemaModule, `with${string}`>]: {
-				execute(): Promise<unknown>
+		it('should use primary dialect for DDL queries', async () => {
+			const queries = {
+				alterTable: db.schema
+					.alterTable('users')
+					.addColumn('nickname', 'varchar'),
+				createIndex: db.schema
+					.createIndex('users_index')
+					.on('users')
+					.column('email'),
+				createSchema: db.schema.createSchema('moshe'),
+				createTable: db.schema
+					.createTable('cakes')
+					.addColumn('id', 'serial', (cb) => cb.primaryKey()),
+				createType: db.schema
+					.createType('cake_type')
+					.asEnum(['chocolate', 'vanilla']),
+				createView: db.schema
+					.createView('users_view')
+					.as(db.selectFrom('users').selectAll()),
+				dropIndex: db.schema.dropIndex('users_index'),
+				dropSchema: db.schema.dropSchema('moshe'),
+				dropTable: db.schema.dropTable('cakes'),
+				dropType: db.schema.dropType('cake_type'),
+				dropView: db.schema.dropView('users_view'),
+			} satisfies {
+				[K in keyof Omit<SchemaModule, `with${string}`>]: {
+					execute(): Promise<unknown>
+				}
 			}
-		}
 
-		await Promise.all(Object.values(queries).map((query) => query.execute()))
+			await Promise.all(Object.values(queries).map((query) => query.execute()))
 
-		expect(executions).toEqual(Object.values(queries).map(() => 'primary'))
-	})
+			expect(executions).toEqual(Object.values(queries).map(() => 'primary'))
+		})
 
-	it('should use primary dialect for raw queries', async () => {
-		await sql`select 1`.execute(db)
+		it('should use primary dialect for raw queries', async () => {
+			await sql`select 1`.execute(db)
 
-		expect(executions).toEqual(['primary'])
-	})
+			expect(executions).toEqual(['primary'])
+		})
 
-	it('should use primary dialect for DML queries that mutate data', async () => {
-		const queries = {
-			deleteFrom: db.deleteFrom('users').where('id', '=', 1),
-			insertInto: db
-				.insertInto('users')
-				.values({ email: 'info@example.com', is_verified: false }),
-			mergeInto: db
-				.mergeInto('users as u1')
-				.using('users as u2', 'u1.id', 'u2.id')
-				.whenMatched()
-				.thenDoNothing(),
-			replaceInto: db
-				.replaceInto('users')
-				.values({ email: 'info@example.com', is_verified: false }),
-			updateTable: db
-				.updateTable('users')
-				.set('is_verified', true)
-				.where('id', '=', 1),
-			with: db
-				.with('insert', (qb) =>
-					qb
-						.insertInto('users')
-						.values({ email: 'info@example.com', is_verified: false })
-						.returning('id'),
-				)
-				.selectFrom('users')
-				.innerJoin('insert', 'insert.id', 'users.id')
-				.selectAll(),
-		} satisfies {
-			[K in
-				| keyof Omit<
-						QueryCreator<Database>,
-						`select${string}` | `with${string}`
-				  >
-				| 'with']: {
-				execute(): Promise<unknown>
+		it('should use primary dialect for DML queries that mutate data', async () => {
+			const queries = {
+				deleteFrom: db.deleteFrom('users').where('id', '=', 1),
+				insertInto: db
+					.insertInto('users')
+					.values({ email: 'info@example.com', is_verified: false }),
+				mergeInto: db
+					.mergeInto('users as u1')
+					.using('users as u2', 'u1.id', 'u2.id')
+					.whenMatched()
+					.thenDoNothing(),
+				replaceInto: db
+					.replaceInto('users')
+					.values({ email: 'info@example.com', is_verified: false }),
+				updateTable: db
+					.updateTable('users')
+					.set('is_verified', true)
+					.where('id', '=', 1),
+				with: db
+					.with('insert', (qb) =>
+						qb
+							.insertInto('users')
+							.values({ email: 'info@example.com', is_verified: false })
+							.returning('id'),
+					)
+					.selectFrom('users')
+					.innerJoin('insert', 'insert.id', 'users.id')
+					.selectAll(),
+			} satisfies {
+				[K in
+					| keyof Omit<
+							QueryCreator<Database>,
+							`select${string}` | `with${string}`
+					  >
+					| 'with']: {
+					execute(): Promise<unknown>
+				}
 			}
+
+			await Promise.all(Object.values(queries).map((query) => query.execute()))
+
+			expect(executions).toEqual(Object.values(queries).map(() => 'primary'))
+		})
+
+		it('should use primary dialect for transactions', async () => {
+			await db.transaction().execute(async (trx) => {
+				await trx.selectFrom('users').selectAll().execute()
+			})
+
+			expect(executions).toEqual(['primary'])
+		})
+
+		it('should use replica dialects for DML queries that do not mutate data', async () => {
+			const queries = {
+				selectFrom: db.selectFrom('users').selectAll(),
+				selectNoFrom: db
+					.selectNoFrom((eb) => eb.selectFrom('users').selectAll().as('u'))
+					.selectAll(),
+				with: db
+					.with('u1', (qb) => qb.selectFrom('users').selectAll())
+					.selectFrom('u1')
+					.selectAll(),
+			} satisfies {
+				[K in keyof Pick<
+					QueryCreator<Database>,
+					'selectFrom' | 'selectNoFrom' | 'with'
+				>]: { execute(): Promise<unknown> }
+			}
+
+			// with extra queries to test round-robin
+			await Promise.all([
+				Object.values(queries).map((query) => query.execute()),
+				db.selectFrom('users').selectAll().execute(),
+				db.selectFrom('users').selectAll().execute(),
+			])
+
+			replicaAssertion(executions)
+		})
+
+		const message =
+			'KyselyReplication: transaction started with replica connection!'
+
+		if (onTransaction === 'error') {
+			it('should throw an error when a transaction is started with a replica connection', async () => {
+				await expect(
+					db.connection().execute(async (con) => {
+						await con.selectFrom('users').selectAll().execute()
+
+						await con.transaction().execute(async (trx) => {
+							await trx.selectFrom('users').selectAll().execute()
+						})
+					}),
+				).rejects.toThrow(message)
+			})
+		} else {
+			it('should warn when a transaction is started with a replica connection', async () => {
+				await db.connection().execute(async (con) => {
+					await con.selectFrom('users').selectAll().execute()
+
+					await con.transaction().execute(async (trx) => {
+						await trx.selectFrom('users').selectAll().execute()
+					})
+				})
+
+				expect(warnSpy).toHaveBeenCalledTimes(1)
+				expect(warnSpy).toHaveBeenCalledWith(message)
+			})
 		}
-
-		await Promise.all(Object.values(queries).map((query) => query.execute()))
-
-		expect(executions).toEqual(Object.values(queries).map(() => 'primary'))
-	})
-
-	it('should use replica dialects for DML queries that do not mutate data', async () => {
-		const queries = {
-			selectFrom: db.selectFrom('users').selectAll(),
-			selectNoFrom: db
-				.selectNoFrom((eb) => eb.selectFrom('users').selectAll().as('u'))
-				.selectAll(),
-			with: db
-				.with('u1', (qb) => qb.selectFrom('users').selectAll())
-				.selectFrom('u1')
-				.selectAll(),
-		} satisfies {
-			[K in keyof Pick<
-				QueryCreator<Database>,
-				'selectFrom' | 'selectNoFrom' | 'with'
-			>]: { execute(): Promise<unknown> }
-		}
-
-		await Promise.all([
-			Object.values(queries).map((query) => query.execute()),
-			db.selectFrom('users').selectAll().where('id', '=', 1).execute(),
-			db.selectFrom('users').selectAll().where('id', '=', 2).execute(),
-		])
-
-		expect(executions).toEqual([
-			'replica-0',
-			'replica-1',
-			'replica-2',
-			'replica-0',
-			'replica-1',
-		])
-	})
-})
+	},
+)
 
 function getDummyDialect(name: string, executions: string[]): Dialect {
 	return {
