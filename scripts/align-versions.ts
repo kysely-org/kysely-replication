523f7807f2b967c77d77b27ac169551773e25b69
Igal Klebanov <igalklebanov@gmail.com>
2026-07-06 04:55:32 +0300

    ...

--- scripts/align-versions.ts
+++ scripts/align-versions.ts
@@ -1,8 +1,8 @@
 /**
- * This script aligns docs site package.json AND jsr versions with
- * Kysely's version so we use only the latest published version in the docs and JSR publish.
+ * This script aligns jsr versions with kysely-replication's version so we use only
+ * the latest published version in JSR publish.
  */
-import { writeFileSync } from 'node:fs'
+import { readFile, writeFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 import { dirname, join } from 'pathe'
 import jsrJson from '../jsr.json' with { type: 'json' }
@@ -10,19 +10,22 @@ import pkgJson from '../package.json' with { type: 'json' }
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const { devDependencies, version } = pkgJson
+const { version } = pkgJson
 
-writeFileSync(
-	join(__dirname, '../deno.json'),
-	`${JSON.stringify(
-		{
-			...jsrJson,
-			imports: {
-				kysely: `jsr:@kysely/kysely@${devDependencies.kysely.replace('^', '')}`,
-			},
-			version,
-		},
-		null,
-		2,
-	)}\n`,
-)
+const readmePath = join(__dirname, '../README.md')
+
+const readmeContent = await readFile(readmePath, { encoding: 'utf8' })
+
+await Promise.all([
+	writeFile(
+		join(__dirname, '../jsr.json'),
+		`${JSON.stringify({ ...jsrJson, version }, null, 2)}\n`,
+	),
+	writeFile(
+		readmePath,
+		readmeContent.replace(
+			/([^\d]?)\d{1,2}\.\d{1,2}\.\d{1,2}(-rc\.\d+)?([^\d]?)/g,
+			`$1${version}$3`,
+		),
+	),
+])
